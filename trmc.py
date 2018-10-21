6aa196743d35891b5891a72c12fc2085baa02a2f
Lee Aspitarte <aspitarte@gmail.com>
2018-10-21 14:32:15 -0700

    add extracting and plotting of maxg and fom

--- trmc.py
+++ trmc.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+import re
 
 def load(filepaths, offsettime = None, sub_lowpow = False):
-    
+
     params = pd.read_csv(filepaths[0], nrows = 11, usecols = [1])
     params = params.transpose()
     amp = float(params['Amplification'][0]) # Is amplification already taken into account?
@@ -15,14 +16,20 @@ def load(filepaths, offsettime = None, sub_lowpow = False):
     time = pd.read_csv(filepaths[0], skiprows = 13)['Time(s)']
     V1 = pd.read_csv(filepaths[-1], skiprows = 13)['Voltage (V)']
     df_V= pd.DataFrame(index = time)
-
+    fluences = pd.Series(index = filepaths)
     for filepath in filepaths:
+
         temp = pd.read_csv(filepath, skiprows = 13)
         volt = temp['Voltage (V)']
         if(sub_lowpow):
             volt = volt -V1
         df_V = pd.concat([df_V, volt], axis = 1)
 
+        m = re.search('Fluence=(.+?)_',filepath) # Read fluence from filename
+        if m:
+            fluences[filepath] = m.group(1)
+
+    df_V.columns = fluences
     if offsettime is not None:
         df_V = df_V - np.mean(df_V[0:offsettime])
 
@@ -30,6 +37,29 @@ def load(filepaths, offsettime = None, sub_lowpow = False):
 
     return df_V, df_cond
 
+def load_fluence(filepath):
+    """Fluence file was not included in all experiments?"""
+    filepath = 'C:\\Users\\aspit\\OneDrive\\Data\\TRMC\\Gratzel\\Sample A\\High_Power_3_FluenceSweep.csv'
+    fluencesweep = pd.read_csv(filepath, skiprows = 13)
+    fluences = fluencesweep['Fluence(cm^-2)']
+    return fluences
+
 def convert_V2cond(df_V,back_V,K):
     df_cond = - ((df_V)/back_V)/K
     return df_cond
+
+def maxG_and_fom(df_cond):
+    beta = 2.25
+    e = 1.6e-19
+    FA = 1
+    M = 0.25
+
+    fluences = df_cond.columns
+    maxG = pd.Series(index = fluences)
+    fom = pd.Series(index = fluences)
+
+    for fluence in fluences:
+        maxG[fluence] = df_cond[fluence].max()
+        fom[fluence] =  maxG[fluence]/(beta*e*fluence*FA*M)
+
+    return maxG, fom
